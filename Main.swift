372cabe5a989140274a1d627cfae537838d0cab2
Jaswanth Ch <chennu.siva@gmail.com>
2025-06-11 15:55:51 +0530

    Update Main.swift

--- Main.swift
+++ Main.swift
@@ -1,12 +1,27 @@
+/*
+ * SwiftyArduino
+ * Provides arduino-like syntax and library support for swift embedded
+ * This example code blinks the built in LED on the RPI pico/pico 2
+ *
+ * By Jaswanth Venkata Sai Chennu
+ * On 11 / 06 / 2025
+*/
+
 @main
 struct Main {
   static func main() {
-    let ledPin: UInt32 = UInt32(PICO_DEFAULT_LED_PIN)
+    // Define a variable for the LED GPIO pin
+    let ledPin: UInt32 = 25
+    // Set the pin output
     pinMode(pin: ledPin, mode: true)
     while true {
+      // turn on the LED
       digitalWrite(pin: ledPin, value: true)
+      // wait one second
       delay(time: 1000)
+      // turn off the LED
       digitalWrite(pin: ledPin, value: false)
+      // wait one second
       delay(time: 1000)
     }
   }
